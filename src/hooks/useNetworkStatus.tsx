7e0c431a51078872a19d298bec14edfa1dde2a84
Saipavan Tej <saipavantej@gmail.com>
2024-01-04 01:13:47 +0530

    added hooks

---
+++ src/hooks/useNetworkStatus.tsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+import NetInfo from '@react-native-community/netinfo';
+
+const useNetworkStatus = () => {
+  const [isConnected, setIsConnected] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+    });
+
+    NetInfo.fetch().then(state => {
+      setIsConnected(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return isConnected;
+};
+
+export default useNetworkStatus;
